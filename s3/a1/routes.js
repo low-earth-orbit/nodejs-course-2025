9da0848cfeac36266b480e398b69bb88f79a6c6f
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-10-08 01:54:12 -0300

    add assignment 1

---
+++ s3/a1/routes.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Greetings</title></head>");
+    res.write("<body><p>Hello world!</p>");
+    res.write(
+      '<body><form action="/create-user" method="POST">Add a user: <input type="text" name="username"><button type="submit">Add</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/users") {
+    res.write("<html>");
+    res.write("<head><title>Greetings</title></head>");
+    res.write("<body><p>Users</p>");
+    res.write("<body><ul><li>User 1</li><li>User 2</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+exports.handler = requestHandler;
