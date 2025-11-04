67f4898efd907cdec27c55e3fcd7dff71b770ad2
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-11-03 21:49:07 -0400

    65 parsing incoming requests

--- s5/03-handling-different-routes/app.js
+++ s5/03-handling-different-routes/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.use(bodyParser.urlencoded());
+
+app.use("/", (req, res, next) => {
   console.log("This always runs!");
-  next(); // Call the next middleware
+  next();
 });
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></input></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use('/', (req, res, next) => {
