defe588c452e401de9d9dc0e966c38edd49aba21
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-11-11 23:16:11 -0400

    71, 72

--- s5/05-using-express-router/app.js
+++ s5/05-using-express-router/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,12 +10,12 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // 404 page
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
