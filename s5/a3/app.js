4a91ac062c89f400fae17d455a623772148d20f4
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-11-17 01:24:19 -0400

    assignment 3

---
+++ s5/a3/app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const mainRoutes = require("./routes/main");
+const usersRoutes = require("./routes/users");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(mainRoutes);
+app.use(usersRoutes);
+
+app.listen(3000);
